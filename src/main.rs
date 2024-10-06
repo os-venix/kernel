a58fa9a121aa3fcd6ca2bcd1cb0f36f578c58ef6
Connor Wood <connorwood71@gmail.com>
2024-10-06 17:44:49 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+use bootloader_api;
+use core::panic::PanicInfo;
+use conquer_once::spin::OnceCell;
+use printk;
+
+const CONFIG: bootloader_api::BootloaderConfig = {
+    let mut config = bootloader_api::BootloaderConfig::new_default();
+    config.kernel_stack_size = 100 * 1024; // 100 KiB
+    config
+};
+bootloader_api::entry_point!(kernel_main, config = &CONFIG);
+
+pub static PRINTK: OnceCell<printk::LockedPrintk> = OnceCell::uninit();
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+	let info = framebuffer.info();
+	let buffer = framebuffer.buffer_mut();
+
+	let kernel_logger = PRINTK.get_or_init(move || printk::LockedPrintk::new(buffer, info));
+	log::set_logger(kernel_logger).expect("Logger already set");
+	log::set_max_level(log::LevelFilter::Trace);
+	log::info!("Hellorld");
+    } else {
+	panic!();
+    }
+
+    loop {}
+}
