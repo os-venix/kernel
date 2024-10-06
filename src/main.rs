59d2ece851c6dff016af8ebdcb26ce0a07b3840b
Connor Wood <connorwood71@gmail.com>
2024-10-06 22:07:24 +0100

    Added CPU exceptions for breakpoint, double fault.
    Double fault uses a dedicated stack, stored in the TSS of the new kernel GDT. This allows us to safely handle
    stack overflows in the kernel, outputting useful debugging information when panicking.
    
    Furthermore, outfit panic handler to print debugging information before halting with an infinite loop. This means
    that should Rust runtime ever cause a panic that does not trigger a CPU exception, useful debugging information
    will also be output.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
 use bootloader_api;
 use core::panic::PanicInfo;
 use conquer_once::spin::OnceCell;
 use printk;
+
+mod interrupts;
+mod gdt;
 
 const CONFIG: bootloader_api::BootloaderConfig = {
     let mut config = bootloader_api::BootloaderConfig::new_default();
@@ -16,11 +20,12 @@ bootloader_api::entry_point!(kernel_main, config = &CONFIG);
 pub static PRINTK: OnceCell<printk::LockedPrintk> = OnceCell::uninit();
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    log::error!("{}", info);
     loop {}
 }
 
-fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+fn init(boot_info: &'static mut bootloader_api::BootInfo) {
     if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
 	let info = framebuffer.info();
 	let buffer = framebuffer.buffer_mut();
@@ -28,10 +33,16 @@ fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
 	let kernel_logger = PRINTK.get_or_init(move || printk::LockedPrintk::new(buffer, info));
 	log::set_logger(kernel_logger).expect("Logger already set");
 	log::set_max_level(log::LevelFilter::Trace);
-	log::info!("Hellorld");
     } else {
 	panic!();
     }
 
+    log::info!("Initialising CPU0...");
+    gdt::init();
+    interrupts::init_idt();
+}
+
+fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    init(boot_info);
     loop {}
 }
