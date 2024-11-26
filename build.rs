74579b9dbf0b7f2a4550adee81be6369fbed3600
Connor Wood <connorwood71@gmail.com>
2024-11-26 16:20:25 +0000

    Compile kernel statically for Limine

--- build.rs
+++ build.rs
@@ -4,4 +4,5 @@ fn main() {
     let linker_script = PathBuf::from(std::env::current_dir().unwrap()).join("linker.ld");
 
     println!("cargo:rustc-link-arg=-T{}", linker_script.display());
+    println!("cargo:rustc-link-arg=-static");
 }
